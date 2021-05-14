c56b60d93d68b50f5de995a2bcc3cc387f950a8b
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-14 22:42:07 +0400

    2. getArticlesByAuthor finished

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
 const articles = [{
         id: 1,
         title: 'How I learn coding?',
@@ -23,7 +25,15 @@ const articles = [{
     },
 ];
 
+app.get("/articles/search_1", (req, res, next) => {
+    const authorName = req.query.author;
 
+    const byAuthor = articles.filter((element) => {
+        return element.author === authorName;
+    });
+    res.status = 200;
+    res.json(byAuthor);
+});
 
 app.get("/articles", (req, res, next) => {
     res.status = 200;
