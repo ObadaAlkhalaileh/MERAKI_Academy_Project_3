6c06fdd5327a0e91bf223af0c1cf9f1b3d271268
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-14 22:19:42 +0400

    1. getAllArticles finished

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const articles = [{
+        id: 1,
+        title: 'How I learn coding?',
+        description: 'Lorem, Quam, mollitia.',
+        author: 'Jouza',
+    },
+    {
+        id: 2,
+        title: 'Coding Best Practices',
+        description: 'Lorem, ipsum dolor sit, Quam, mollitia.',
+        author: 'Besslan',
+    },
+    {
+        id: 3,
+        title: 'Debugging',
+        description: 'Lorem, Quam, mollitia.',
+        author: 'Jouza',
+    },
+];
 
-app.get("/", (req, res, next) => {
-    res.json("hello from server");
+
+
+app.get("/articles", (req, res, next) => {
+    res.status = 200;
+    res.json(articles);
 });
 
 app.listen(PORT, () => {
