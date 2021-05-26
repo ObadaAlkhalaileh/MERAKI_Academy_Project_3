5aaf5c07e4db56b5161fce6bb2022379cda5a222
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-27 01:45:49 +0400

    1. register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <p>App</p>
+    <div className='app'>
+      <div className='header'>
+      <span>Login</span> <span>Register</span>
+    </div>
+    <form className='register'>
+      <label>Register:</label>
+      <input type='text' placeholder='firstName here '/>
+      <input type='text' placeholder='lastName here '/>
+      <input type='number' placeholder='age here '/>
+      <input type='text' placeholder='country here '/>
+      <input type='text' placeholder='email here '/>
+      <input type='password' placeholder='password here '/>
+      <button>Register</button>
+    </form>
     </div>
   );
 }
